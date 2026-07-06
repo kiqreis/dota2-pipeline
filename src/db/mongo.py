481dc73ae0adf7ace4c80e4ded1ebe457a840438
Kaique Reis <kreis1265@gmail.com>
2026-07-06 17:26:24 -0300

    refactor: adjusting the mongo connection to avoid timeout errors

--- src/db/mongo.py
+++ src/db/mongo.py
@@ -4,7 +4,15 @@ from src.shared.settings import Settings
 
 settings = Settings()
 
-mongo_client = MongoClient(settings.MONGO_DB_URI)
+mongo_client = MongoClient(
+    settings.MONGO_DB_URI,
+    maxPoolSize=50,
+    connectTimeoutMS=20000,
+    socketTimeoutMS=30000,
+    retryWrites=True,
+    serverSelectionTimeoutMS=20000,
+)
+
 mongo_db_name = mongo_client.get_database(settings.MONGO_DB_NAME)
 
 match_details_collection = mongo_db_name.get_collection("match_details")
