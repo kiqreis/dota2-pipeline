9dd36ad54d8977b35404c806f946f8b3547399bb
Kaique Reis <kreis1265@gmail.com>
2026-09-15 16:16:16 -0300

    test: add database engine fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,22 @@
 import pytest
 
+from sqlalchemy import create_engine
+from collect.models import Base
+
 from src.shared.settings import Settings
 
 
 @pytest.fixture(scope="session")
 def settings():
     return Settings()
+
+
+@pytest.fixute(scope="session")
+def engine(settings):
+    engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    yield engine
+
+    Base.metadata.drop_all(engine)
+    engine.dispose()
