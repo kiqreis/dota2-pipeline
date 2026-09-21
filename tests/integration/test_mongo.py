ad2156ad1af71d539f267ebf3c25302fb80443e3
Kaique Reis <kreis1265@gmail.com>
2026-09-20 22:28:12 -0300

    test: verify MongoDb match_id index is unique

--- tests/integration/test_mongo.py
+++ tests/integration/test_mongo.py
@@ -5,3 +5,13 @@ pytestmark = pytest.mark.integration
 
 def test_when_collection_created_then_name_test_prefix(mongo_collection):
     assert mongo_collection.name.startswith("match_details_test")
+
+
+def test_when_collection_created_then_match_id_index_is_unique(mongo_collection):
+    index = next(
+        (i for i in mongo_collection.list_indexes() if i["name"] == "match_id_1"),
+        None,
+    )
+
+    assert index is not None
+    assert index["unique"] is True
